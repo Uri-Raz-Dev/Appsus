bf7bc20271821408298620c29e79e5fc0f4586a9
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-05-29 20:26:37 +0300

    Fix EmailCompose

--- apps/mail/cmps/EmailFolderList.jsx
+++ apps/mail/cmps/EmailFolderList.jsx
@@ -1,9 +1,7 @@
 
-const Router = ReactRouterDOM.HashRouter
-const { Routes, Route } = ReactRouterDOM
-const { NavLink, Link } = ReactRouterDOM
-import { EmailIcons } from "./EmailIcons.jsx";
-
+const { Link } = ReactRouterDOM
+import { EmailIcons } from "./EmailIcons.jsx"
+import { EmailCompose } from "./EmailCompose.jsx"
 
 export function EmailFolderList({ folder, unreadInboxCount, closeCompose, onSendMail, openCompose, isComposeOpen }) {
     return (
@@ -13,16 +11,33 @@ export function EmailFolderList({ folder, unreadInboxCount, closeCompose, onSend
                 <div className="compose">Compose</div>
             </Link>
             <Link replace to={''}>{isComposeOpen && <EmailCompose closeCompose={closeCompose} onSendMail={onSendMail} />}</Link>
+
             <ul className="links-wrapper">
-                <li className={`${folder === 'inbox' ? 'active' : ''}`}>  <span className={`folder-icon`}>{EmailIcons('inbox')}</span><Link to="/mail/inbox">Inbox</Link><span>{unreadInboxCount}</span></li>
+                <li className={`${folder === 'inbox' ? 'active' : ''}`}>
+                    <span className={`folder-icon`}>{EmailIcons('inbox')}</span>
+                    <Link to="/mail/inbox">Inbox</Link>
+                    <span>{unreadInboxCount}</span>
+                </li>
 
-                <li className={`${folder === 'starred' ? 'active' : ''}`}> <span className={`folder-icon`}>{EmailIcons('starred')}</span><Link to="/mail/starred">Starred</Link></li>
+                <li className={`${folder === 'starred' ? 'active' : ''}`}>
+                    <span className={`folder-icon`}>{EmailIcons('starred')}</span>
+                    <Link to="/mail/starred">Starred</Link>
+                </li>
 
-                <li className={`${folder === 'sent' ? 'active' : ''}`}><span className={`folder-icon`}>{EmailIcons('sent')}</span><Link to="/mail/sent">Sent</Link></li>
+                <li className={`${folder === 'sent' ? 'active' : ''}`}>
+                    <span className={`folder-icon`}>{EmailIcons('sent')}</span>
+                    <Link to="/mail/sent">Sent</Link>
+                </li>
 
-                <li className={`${folder === 'draft' ? 'active' : ''}`}><span className={`folder-icon`}>{EmailIcons('draft')}</span> <Link to="/mail/draft">Draft</Link></li>
+                <li className={`${folder === 'draft' ? 'active' : ''}`}>
+                    <span className={`folder-icon`}>{EmailIcons('draft')}</span>
+                    <Link to="/mail/draft">Draft</Link>
+                </li>
 
-                <li className={`${folder === 'trash' ? 'active' : ''}`}><span className={`folder-icon`}>{EmailIcons('trash')}</span><Link to="/mail/trash">Trash</Link></li>
+                <li className={`${folder === 'trash' ? 'active' : ''}`}>
+                    <span className={`folder-icon`}>{EmailIcons('trash')}</span>
+                    <Link to="/mail/trash">Trash</Link>
+                </li>
             </ul>
         </nav>
     );
