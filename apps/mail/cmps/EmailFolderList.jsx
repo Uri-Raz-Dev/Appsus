c9c181b1302e9262b06c9a80623373f45d35dbbe
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-06-01 17:33:01 +0300

    Change AppHeader style

--- apps/mail/cmps/EmailFolderList.jsx
+++ apps/mail/cmps/EmailFolderList.jsx
@@ -10,37 +10,38 @@ export function EmailFolderList({ folder, unreadInboxCount, closeCompose,
     return (
         <nav className={isCollapse ? "email-folder-list flex collapse" : "email-folder-list flex"}>
             <span className="hambar" onClick={() => toggleCollapse()}>☰</span>
+
             <Link onClick={openCompose} className="compose-wrapper flex" to="compose">
                 <span className="compose-icon">{EmailIcons('compose')}</span>
-                {!isCollapse && <div className="compose">Compose</div>}
+                {!isCollapse && <div className="compose folderName">Compose</div>}
             </Link>
             <Link replace to={''}>{isComposeOpen && <EmailCompose closeCompose={closeCompose} onSendMail={onSendMail} />}</Link>
 
             <ul className="links-wrapper">
                 <li className={`${folder === 'inbox' ? 'active' : ''}`}>
                     <Link to="/mail/inbox" className={`folder-icon`}>{EmailIcons('inbox')}</Link>
-                    {!isCollapse && <Link to="/mail/inbox">Inbox</Link>}
-                    {!isCollapse && <span>{unreadInboxCount}</span>}
+                    {!isCollapse && <Link className="folderName" to="/mail/inbox">Inbox</Link>}
+                    {!isCollapse && <span className="folderName">{unreadInboxCount}</span>}
                 </li>
 
                 <li className={`${folder === 'starred' ? 'active' : ''}`}>
                     <Link to="/mail/starred" className={`folder-icon`}>{EmailIcons('starred')}</Link>
-                    {!isCollapse && <Link to="/mail/starred">Starred</Link>}
+                    {!isCollapse && <Link className="folderName" to="/mail/starred">Starred</Link>}
                 </li>
 
                 <li className={`${folder === 'sent' ? 'active' : ''}`}>
                     <Link to="/mail/sent" className={`folder-icon`}>{EmailIcons('sent')}</Link>
-                    {!isCollapse && <Link to="/mail/sent">Sent</Link>}
+                    {!isCollapse && <Link className="folderName" to="/mail/sent">Sent</Link>}
                 </li>
 
                 <li className={`${folder === 'draft' ? 'active' : ''}`}>
                     <Link to="/mail/draft" className={`folder-icon`}>{EmailIcons('draft')}</Link>
-                    {!isCollapse && <Link to="/mail/draft">Draft</Link>}
+                    {!isCollapse && <Link className="folderName" to="/mail/draft">Draft</Link>}
                 </li>
 
                 <li className={`${folder === 'trash' ? 'active' : ''}`}>
                     <Link to="/mail/trash" className={`folder-icon`}>{EmailIcons('trash')}</Link>
-                    {!isCollapse && <Link to="/mail/trash">Trash</Link>}
+                    {!isCollapse && <Link className="folderName" to="/mail/trash">Trash</Link>}
                 </li>
             </ul>
         </nav>
