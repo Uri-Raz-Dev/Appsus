283daf11cf7eb6abb873001ff94d3145395fffa5
ori-milstein <mydoal@keemail.me>
2024-06-02 20:16:39 +0300

    Cleanup NotePreview

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -8,23 +8,39 @@ const { useState, useEffect } = React
 export function NotePreview({ note }) {
     const { type } = note
     const [noteForPreview, setNoteForPreview] = useState(note)
+    const info = noteForPreview.info
+
     useEffect(() => {
+
         const unsubscribe = eventBusService.on('saveEdit', note => {
             setNoteForPreview(note)
         })
+
         return unsubscribe
     }, [])
 
 
-    return (type === 'NoteTodos') ? <CheckBoxPreview note={note} />
-        : <section className="content">
-            {(noteForPreview.info.title) && <h3>{noteForPreview.info.title}</h3>}
-            <p >{(noteForPreview.info.txt || noteForPreview.info.title || noteForPreview.info.url) ? noteForPreview.info.txt : 'Empty note'}</p>
-        </section>
+    return (
+        (type === 'NoteTodos') ?
+            <CheckBoxPreview note={note} />
+            :
+            <section className="content">
+                {(noteForPreview.info.title) &&
+                    <h3>{info.title}</h3>
+                }
+
+                <p >{(info.txt || info.title || info.url) ?
+                    info.txt : 'Empty note'
+                }
+                </p>
+            </section>
+    )
 }
 
 export function CheckBoxPreview({ note }) {
     const [newNote, setNewNote] = useState(note)
+    const info = note.info
+    const newInfo = newNote.info
 
     useEffect(() => {
         const unsubscribe = eventBusService.on('saveToDoEdit', note => {
@@ -34,33 +50,43 @@ export function CheckBoxPreview({ note }) {
     }, [])
 
 
-    useEffect(() => { if (newNote.info) noteService.save(newNote) }, [newNote])
+    useEffect(() => { if (newInfo) noteService.save(newNote) }, [newNote])
 
     function toggleDone(todo) {
+
         setNewNote(prev => {
             let nextToDos = [...prev.info.todos]
-            nextToDos[newNote.info.todos.indexOf(todo)] = { ...todo, doneAt: (todo.doneAt === null) ? 1 : null }
+            nextToDos[newInfo.todos.indexOf(todo)] =
+                { ...todo, doneAt: (todo.doneAt === null) ? 1 : null }
             let newInfo = { ...prev.info, todos: nextToDos }
             return { ...prev, info: newInfo }
         })
+
     }
 
     if (note.info.todos.length) {
-        return <section className="content todos">
-            {(note.info.title) && <h3>{note.info.title}</h3>}
-            {note.info.todos.length > 0 &&
-                <ul>
-                    {
-                        (newNote.info) && newNote.info.todos.map(todo =>
-                            <li key={utilService.makeId()} className={(todo.doneAt === null) ? 'checkbox' : 'box-checked'}>
-                                <Icons todo={todo} toggleDone={toggleDone} type={(todo.doneAt === null) ? 'checkBox' : 'boxChecked'} />
-                                {todo.txt}
-                            </li>
-                        )
-                    }
-                </ul>
-            }
-        </section>
+        return (
+            <section className="content todos">
+                {(info.title) && <h3>{info.title}</h3>}
+                {info.todos.length > 0 &&
+                    <ul>
+                        {
+                            (newInfo) && newInfo.todos.map(todo =>
 
+                                <li key={utilService.makeId()}
+                                    className={(todo.doneAt === null) ? 'checkbox' : 'box-checked'}>
+
+                                    <Icons todo={todo}
+                                        toggleDone={toggleDone}
+                                        type={(todo.doneAt === null) ? 'checkBox' : 'boxChecked'} />
+                                    {todo.txt}
+
+                                </li>
+                            )
+                        }
+                    </ul>
+                }
+            </section>
+        )
     }
 }
