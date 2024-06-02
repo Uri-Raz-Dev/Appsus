45009c1cb1aa6e51ca75b6b19c713e14caab4b6d
ori-milstein <mydoal@keemail.me>
2024-06-02 19:36:18 +0300

    Cleanup icons

--- apps/note/cmps/Icons.jsx
+++ apps/note/cmps/Icons.jsx
@@ -1,7 +1,9 @@
 // import { trash } from "../../../assets/icons/bin.svg";
 export function Icons({ type, toggleDone, todo }) {
     const color = '#202124'
+
     const icons = {
+
         trash: <svg className="icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill={color}><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" /></svg>,
 
         pin: <svg className="icon" xmlns="http://www.w3.org/2000/svg" fill={color} width="24" height="24" viewBox="0 0 24 24">
@@ -33,5 +35,6 @@ export function Icons({ type, toggleDone, todo }) {
         mail: <svg className="icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill={color}><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" /></svg>
 
     }
+
     return icons[type]
 }
