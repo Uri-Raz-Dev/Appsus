9e07edbc5b5a642c1c94dcc35152c49a50d6625c
ori-milstein <mydoal@keemail.me>
2024-06-03 11:47:11 +0300

    Cleanup Add to Do

--- apps/note/cmps/AddToDo.jsx
+++ apps/note/cmps/AddToDo.jsx
@@ -1,5 +1,4 @@
 import { eventBusService } from "../../../services/event-bus.service.js"
-import { utilService } from "../../../services/util.service.js"
 import { noteService } from '../services/note.service.js'
 
 const { useState, useEffect } = React
@@ -8,45 +7,15 @@ export function AddToDo({ makeNewNotes }) {
     const [isOpen, setIsOpen] = useState(true)
     const [isButton, setIsButton] = useState('')
 
-    const [newNote, setNewNote] = useState(noteService.getEmptyTodoNote())
     const [newToDos, setNewToDos] = useState(noteService.getEmptyTodos())
     const [newToDo, setNewToDo] = useState(noteService.getEmptyTodo(newToDos.info.todos[0].id))
-    const [isFirstInput, setIsFirstInput] = useState(true)
     const [lineNum, setLineNum] = useState(0)
-    // var lineNum = 0
-
-    // useEffect(() => {
-    //     console.log('newToDos', newToDos)
-    //     // setNewToDos(prev => {
-    //     //     let nextToDos = [...prev.info.todos]
-    //     //     nextToDos[lineNum] = newToDo
-    //     //     let newInfo = { ...prev.info, todos: nextToDos }
-    //     //     console.log('{ ...prev, info: newInfo }', { ...prev, info: newInfo })
-    //     //     return { ...prev, info: newInfo }
-    //     // })
-    //     console.log('lineNum from useeffect', lineNum)
-
-    //     if (newToDos.info.todos[lineNum].txt.length === 1 && lineNum === newToDos.info.todos.length - 1) {
-    //         console.log('hello')
-    //         setNewToDos(prev => {
-    //             let nextToDos = [...prev.info.todos]
-    //             nextToDos.push(noteService.getEmptyTodo())
-    //             let newInfo = { ...prev.info, todos: nextToDos }
-    //             return { ...prev, info: newInfo }
-    //         })
-    //     }
-    // }, [newToDos])
 
     useEffect(() => {
-        console.log('lineNum from linenum', lineNum)
-        console.log('newToDos.info.todos[lineNum].txt.length', newToDos.info.todos[lineNum].txt.length)
-        console.log('newToDos.info.todos.length', newToDos.info.todos.length)
-        console.log('newToDos.info.todos[lineNum].txt.length === 1', newToDos.info.todos[lineNum].txt.length === 1)
-        console.log('lineNum === newToDos.info.todos.length - 1', lineNum === newToDos.info.todos.length - 1)
-        console.log('typeof(lineNum)', typeof (lineNum))
-        console.log('typeof(newToDos.info.todos.length - 1)', typeof (newToDos.info.todos.length - 1))
-        if (newToDos.info.todos[lineNum].txt.length === 1 && +lineNum === newToDos.info.todos.length - 1) {
-            console.log('condition true')
+
+        if (newToDos.info.todos[lineNum].txt.length === 1 &&
+            +lineNum === newToDos.info.todos.length - 1) {
+
             setNewToDos(prev => {
                 let nextToDos = [...prev.info.todos]
                 nextToDos.push(noteService.getEmptyTodo())
@@ -60,11 +29,11 @@ export function AddToDo({ makeNewNotes }) {
         ev.preventDefault()
 
         eventBusService.emit('todo', false)
+
         const toDosForSave = newToDos.info.todos.filter(todo => todo.txt)
         let newInfo = { ...newToDos.info, todos: toDosForSave }
         const noteForSave = { ...newToDos, info: newInfo }
 
-        console.log('newToDos from save', noteForSave)
         noteService.save(noteForSave)
             .then(makeNewNotes)
             .then(() =>
@@ -72,22 +41,14 @@ export function AddToDo({ makeNewNotes }) {
             .catch((result) => {
                 console.log(result);
             })
-            .finally(() => {
-            })
+
     }
 
     function handleChange({ target }) {
         let { value } = target
         setLineNum(target.id)
-        console.log('lineNum from handle', lineNum)
-        // setNewToDo(
-        //     prevNewToDo => {
-        //         console.log('{ ...prevNewToDo, txt: value }', { ...prevNewToDo, txt: value })
-        //         return { ...prevNewToDo, txt: value }
-        //     })
 
         setNewToDos(prev => {
-            console.log('prev', prev)
             let nextToDos = [...prev.info.todos]
             nextToDos[target.id] = { ...newToDo, txt: value }
             let newInfo = { ...prev.info, todos: nextToDos }
@@ -106,15 +67,6 @@ export function AddToDo({ makeNewNotes }) {
                                     <input
                                         type="text"
                                         id={newToDos.info.todos.indexOf(todo)}
-
-                                        // onClick={() => {
-                                        //     console.log('todo.id', todo.id)
-                                        //     console.log('newToDos.info.todos.indexOf(todo)', newToDos.info.todos.indexOf(todo))
-
-                                        //     setLineNum(newToDos.info.todos.indexOf(todo))
-                                        //     setNewToDo(todo)
-                                        // }
-                                        // }
                                         value={todo.txt}
                                         onChange={handleChange}
                                         name="todo" placeholder="List Item"
